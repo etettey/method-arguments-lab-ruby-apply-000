26663ed1ad65bd79a9670ccc793f46210f6e6b07
resourceful-role-9993 <unicetee@gmail.com>
2020-03-15 22:04:14 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -2,3 +2,8 @@ def introduction(name)
   puts "Hi, my name is #{name}."
 end
  introduction("Josh")
+
+def introduction_with_language(name,language)
+  puts "Hi, my name is #{name} and i am learning to program in #{language}."
+  introduction_with_language("Dan","Ember.js")
+  
